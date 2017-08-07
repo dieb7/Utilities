1b306bba60233ea910ce1c6955c6a13589f2164a
Diego Benavides <diegob41@gmail.com>
2017-08-07 16:24:52 -0600

    adding some files to start implementation of Queue

---
+++ tests/Queue/QueueTest.cpp
@@ -0,0 +1,24 @@
+#include "Queue.h"
+
+//CppUTest includes should be after your and system includes
+#include "CppUTest/TestHarness.h"
+
+TEST_GROUP(Queue)
+{
+  Queue* queue;
+
+  void setup()
+  {
+    queue = new Queue();
+  }
+  void teardown()
+  {
+    delete queue;
+  }
+};
+
+TEST(Queue, Create)
+{
+  FAIL("Start here");
+}
+
